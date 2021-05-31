016ebe252e2d138e719040ba2d4124d03712f765
GMCunha <83093980+GMCunha@users.noreply.github.com>
2021-05-31 14:53:44 -0300

    feitos exercicios 6 a 10

--- haskell03.hs
+++ haskell03.hs
@@ -12,3 +12,18 @@ applyExpr l = [3*x+2 | x <- l]
 
 applyExpr' :: [Int] -> [Int]
 applyExpr' l = map (\y -> 3*y+2) l
+
+addSuffix :: String -> [String] -> [String]
+addSuffix str l = [s ++ str | s <- l]
+
+selectgt5 :: [Int] -> [Int]
+selectgt5 l = [x | x <- l, x > 5]
+
+sumOdds :: [Int] -> Int
+sumOdds l = sum [x | x <- l, odd x]
+
+sumOdds' :: [Int] -> Int
+sumOdds' l = sum (filter odd l)
+
+selectExpr :: [Int] -> [Int]
+selectExpr l = [x | x <- l, x >= 20, x <= 50]
