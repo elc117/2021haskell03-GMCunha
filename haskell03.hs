a0c43f06e88831518ba1712cda4d2eebec3a008d
GMCunha <83093980+GMCunha@users.noreply.github.com>
2021-05-31 16:03:57 -0300

    Trabalho concluido!

--- haskell03.hs
+++ haskell03.hs
@@ -26,7 +26,7 @@ sumOdds' :: [Int] -> Int
 sumOdds' l = sum (filter (\ y -> y `mod` 2 == 1) l)
 
 selectExpr :: [Int] -> [Int]
-selectExpr l = [x | x <- l, x >= 20, x <= 50]
+selectExpr l = [x | x <- l, even x, x >= 20, x <= 50]
 
 countShorts :: [String] -> Int
 countShorts l = length [x | x <- l, length x < 5]
@@ -36,3 +36,16 @@ calcExpr l = filter (\ y -> y > 10) [x^2/2 | x <- l]
 
 trSpaces :: String -> String
 trSpaces l = [if x==' ' then '-' else x | x <- l]
+
+selectSnd :: [(Int,Int)] -> [Int]
+selectSnd t = [snd x | x <- t]
+
+dotProd :: [Int] -> [Int] -> Int
+dotProd l1 l2 = sum ([x*y | (x, y) <- zip l1 l2])
+
+{-alternativa sem list comprehension
+mulTuple :: (Int, Int) -> Int
+mulTuple (x, y) = x*y
+
+dotProd :: [Int] -> [Int] -> Int
+dotProd l1 l2 = sum (map mulTuple (zip l1 l2))-}
